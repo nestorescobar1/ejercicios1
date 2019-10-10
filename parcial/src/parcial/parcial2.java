cea6b0dfca368eae2cba2912e33e80ba9202faa2
Nestorp <Nestorp@NESTOR>
2019-10-10 11:14:22 -0500

    parcial

---
+++ parcial/src/parcial/parcial2.java
@@ -0,0 +1,33 @@
+
+package parcial;
+
+  public class {
+    
+  private int commit;
+  private int push;
+  private int pull;
+  private int clase;
+  private int objeto;
+  
+    public int getCommit() {
+        return commit;
+    }
+
+    public int getPush() {
+        return push;
+    }
+
+    public int getPull() {
+        return pull;
+    }
+
+    public int getClase() {
+        return clase;
+    }
+
+    public int getObjeto() {
+        return objeto;
+    }
+
+
+  }
